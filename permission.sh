0aad849c080ce223df411811ca257771f1e60267
Kelvya <kelvya.alandji@supdevinci-edu.fr>
2025-11-03 13:20:41 +0000

    Ajout de PERMISSIONS

---
+++ permission.sh
@@ -0,0 +1,5 @@
+groupadd dev
+useradd -m -g dev bob
+echo "bob:password123" | chpasswd
+echo 'bob ALL=(ALL) NOPASSWD: /usr/bin/apt, /usr/bin/apt-get' > /etc/sudoers.d/bob
+chmod 440 /etc/sudoers.d/bob
