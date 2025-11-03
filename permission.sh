021acaf58e07548a3c80db5a42568692b0c87158
Kelvya <kelvya.alandji@supdevinci-edu.fr>
2025-11-03 13:37:46 +0000

    Ajout de permission.sh

--- permission.sh
+++ permission.sh
@@ -1,5 +1,10 @@
-groupadd dev
-useradd -m -g dev bob
-echo "bob:password123" | chpasswd
-echo 'bob ALL=(ALL) NOPASSWD: /usr/bin/apt, /usr/bin/apt-get' > /etc/sudoers.d/bob
-chmod 440 /etc/sudoers.d/bob
+#!/bin/bash
+creer_bob() {
+    groupadd dev
+    useradd -m -g dev bob
+    echo "bob:password123" | chpasswd
+    echo 'bob ALL=(ALL) NOPASSWD: /usr/bin/apt, /usr/bin/apt-get' > /etc/sudoers.d/bob
+    chmod 440 /etc/sudoers.d/bob
+}
+creer_bob
+
